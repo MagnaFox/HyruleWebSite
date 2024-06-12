ffca9619b2f37065735024855c2ac8c24bd3fad4
Matías Erazo <131489726+MagnaFox@users.noreply.github.com>
2024-06-12 16:11:29 +0000

    Testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
-
-  
+document.getElementById('link').addEventListener('click', function () {
+    var img = document.getElementById('sword-image');
+    if (img.style.display === 'none') {
+        img.style.display = 'block';
+    } else {
+        img.style.display = 'none';
+    }
+});
